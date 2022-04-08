b611ca32ad4be8ac69375f0230e7b6f89849e84e
Thomas McDonald <notaturkey@Thomass-MacBook-Pro.local>
2022-04-08 17:29:18 -0600

    cleanup a bit

--- index.js
+++ index.js
@@ -2,52 +2,66 @@ const express = require('express');
 const app = express();
 const port = 50406;
 var config = require('./config');
-var request = require('request');
 
+const axios = require('axios').default
 
-const e = require('express');
+var runtime = {
+}
+
+function make_api_call(ip, port, user, name){
+    if (runtime[user] == null){
+        runtime[user] = {
+            nodes: {
+            }
+        }
+    }
+    if (runtime[user]['nodes'][name] == null){
+        runtime[user]['nodes'][name] = {
+            'name': name,
+            'node_total': 0,
+            'prev': 0,
+            'curr': 0
+        }
+    }
+
+    var options = {
+        method: 'POST',
+        url: 'http://'+ip+':'+port,
+        headers: {'Content-Type': 'application/json'},
+        data: {id: 1, jsonrpc: '2.0', method: 'creditcoin_hashrate'}
+    };
+
+    axios.request(options).then(function (response) {
+        runtime[user]['nodes'][name]['prev'] = runtime[user]['nodes'][name]['curr']
+        runtime[user]['nodes'][name]['curr'] = response.data.result.hash_count
+        
+        if ( runtime[user]['nodes'][name]['prev']  > runtime[user]['nodes'][name]['curr'] ){
+            runtime[user]['nodes'][name]['prev'] = 0
+        }
+
+        runtime[user]['nodes'][name]['node_total'] += runtime[user]['nodes'][name]['curr'] - runtime[user]['nodes'][name]['prev']
+
+        console.log(runtime[user]);
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
 
 // collect starting num
-function collect_stats(){ 
-    var options = {};
+async function collect_stats(){ 
+    var result;
     for(var user in config.users) {
-        var nodes = config.users[user].nodes;
-        for (var node in nodes){
-            console.log(config.users[user].nodes[node].ip)
-            options = {
-                uri: 'http://'+config.users[user].nodes[node].ip+':'+config.users[user].nodes[node].rpc_port,
-                method: 'POST',
-                json: {"id":1, "jsonrpc":"2.0", "method": "creditcoin_hashrate"}
-            };
-            request(options, function (error, response, body) {
-                if (!error) {
-                    var hashcount = parseInt(body.result.hash_count)
-                    if (config.users[user].nodes[node].curr_hashcount == null){
-                        config.users[user].nodes[node].curr_hashcount = hashcount
-                        console.log(hashcount)
-                    }
-                    else {
-                        config.users[user].nodes[node].prev_hashcount = config.users[user].nodes[node].curr_hashcount;
-                        config.users[user].nodes[node].curr_hashcount = parseInt(hashcount);
-                        
-                        config.users[user].nodes[node].node_total_hashcount += config.users[user].nodes[node].curr_hashcount - config.users[user].nodes[node].prev_hashcount;
-                        console.log('yays')
-                    }
-                }
-                else {
-                    config.users[user].nodes[node].curr_hashcount = null
-                    console.log(error)
-                    console.log(response)
-                }
-            }); 
+        for (var node in config.users[user].nodes){
+            result = await make_api_call(config.users[user].nodes[node].ip, config.users[user].nodes[node].rpc_port, user ,config.users[user].nodes[node].name);
         }
     }
 }
 
+
 var stats_loop = setInterval(collect_stats, 1000);
 
 app.get('/stats', (req, res) => {
-    res.send(config);
+    res.send(runtime);
 });
 
 app.get('/', (req, res) => {
@@ -57,25 +71,17 @@ app.get('/', (req, res) => {
 app.get('/pool', (req, res) => {
     var payout = new Object()
     payout.total_hashes = 0
-    for(var user in config.users) {
-        var user_total = 0;
-        var name = config.users[user].name
-        var nodes = config.users[user].nodes
-        for (var node in nodes){
-            user_total +=  config.users[user].nodes[node].node_total_hashcount
+    for(var user in runtime) {
+        runtime[user].total=0
+        for (var node in runtime[user].nodes){
+            runtime[user].total +=  runtime[user].nodes[node].node_total
         }
-        payout[name] = {num_hashes: user_total}
-        payout.total_hashes +=  user_total
+        payout.total_hashes += runtime[user].total
+        payout[user] = {num_hashes: runtime[user].total}
     }
 
-    for(var user in config.users) {
-        var user_total = 0;
-        var name = config.users[user].name
-        var nodes = config.users[user].nodes
-        for (var node in nodes){
-            user_total +=  config.users[user].nodes[node].node_total_hashcount
-        }
-        payout[name]['percentage'] = (payout[name].num_hashes / payout['total_hashes']) * 100
+    for(var user in payout) {
+        payout[user]['percentage'] = (payout[user].num_hashes / payout['total_hashes']) * 100
     }
 
     res.send(payout);
