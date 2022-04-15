87d555c1199ce9e409748ccb5a7a006d4fa83957
notaturkey <tommytocool5@gmail.com>
2022-04-15 15:17:32 -0600

    minor fixes

--- fe/pool-tracker-fe/pages/index.js
+++ fe/pool-tracker-fe/pages/index.js
@@ -7,44 +7,16 @@ export default function Home() {
   return (
     <>
     <Container>
-    <Row><Navbar> </Navbar></Row>    
-    <Carousel>
-    <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-    </Carousel.Item>
-    </Carousel>
+    <Row><Navbar> </Navbar></Row>
+    <Row className="p-2 m-2 border border-dark rounded " >
+      <Container  className='p-2 bg-light'>
+      <h1>Useful links</h1>
+      <ul>
+        <li><a href='https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Fmainnet.creditcoin.network#/explorer'>block explorer</a></li>
+        <li><a href='https://telemetry.polkadot.io/#list/0xdd954cbf4000542ef1a15bca509cd89684330bee5e23766c527cdb0d7275e9c2'>telemetry</a></li>
+      </ul>
+      </Container>
+    </Row>
     </Container>
     </>
   )
